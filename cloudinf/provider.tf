f43265ae35fb69e3224fb7fdc83b08e5f1414e23
LoneWolf38 <sudodeba97.iiitbbsr@gmail.com>
2019-01-30 05:35:36 +0530

    AWS network created

---
+++ cloudinf/provider.tf
@@ -0,0 +1,14 @@
+provider "aws"{
+	region = "ap-south-1"
+}
+
+provider "kubernetes" {
+  config_context_auth_info = "ops"
+  config_context_cluster   = "mycluster"
+}
+
+resource "kubernetes_namespace" "example" {
+  metadata {
+    name = "my-first-namespace"
+  }
+}
